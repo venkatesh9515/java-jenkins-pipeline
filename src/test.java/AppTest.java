5d4f3c412dabfd6bb8b6fd2d21a57628be1d88c3
Praveen-cheli <chelikanipraveenkumar@gmail.com>
2025-02-09 10:53:21 +0530

    Adding Java Project

---
+++ src/test.java/AppTest.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class AppTest {
+    @Test
+    public void testApp() {
+        assertEquals(2, 1 + 1);
+    }
+}
+
